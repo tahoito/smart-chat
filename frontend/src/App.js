b88fa2e0abdb8f129ae237cd6b6d3b051c78e2cc
tahoito <taho.ito0304@gmail.com>
2025-05-28 22:49:01 +0900

    edit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//今まで送ったメッセージ一覧
+const[chats,SetChats] = useState([]);
+//入力欄、入力更新のメッセージ
+const[newChats, SetNewChats] = useState("");
+
+//API呼び出し
+useEffect(() => {
+  axios.get('http://localhost:8080/api/chats')
+    .then(res => setChats(res.data))
+    .catch(err => console.error(err));
+}, []);
+
+
+const handleSend = () => {
+  //空なら送らない
+  if (!newMessage.trim()) return;
+
+  axios.get('http://localhost:8080/api/chats', {
+    content: NewChats 
+  }).then(res => {
+    setChats([...messages, res.data]);// 新しいメッセージをリストに追加
+    setChats('');
+  }).catch(err => console.error(err));
 }
+
+return(
+  <div style={{ padding: "20px" }}>
+
+    <h2>Smart-chat</h2>
+
+    /** 全メッセージを1つずつ画面に表示 */ 
+    <div>
+      {chats.map((msg,index) =>  (
+        <div key={index}>{msg.content}</div>
+      ))}
+    </div>
+
+    <input 
+      value = {newChat}
+      onChange = {e => setChat(e.target.value)}
+      placeholder="Type a message..."
+    />
+
+    <button onClick={handleSend}>Send</button>
+  </div>
+
+)
 
 export default App;
